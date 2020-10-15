c30c74b59299b29efc445e7860a0e3bfd9b3d9b4
ralphribeiro <ralph_ribeiro@msn.com>
2020-10-15 15:09:45 -0300

    ajuste de nomes

--- src/2 - strings and texts/2.19-writing_a_simple_recursive_descent_parser/example.py
+++ src/2 - strings and texts/2.19-writing_a_simple_recursive_descent_parser/example.py
@@ -1,72 +1,76 @@
-# example.py
-#
-# An example of writing a simple recursive descent parser
+"""
+example.py
+
+An example of writing a simple recursive descent parser
+"""
 
 import re
 import collections
 
 # Token specification
-NUM    = r'(?P<NUM>\d+)'
-PLUS   = r'(?P<PLUS>\+)'
-MINUS  = r'(?P<MINUS>-)'
-TIMES  = r'(?P<TIMES>\*)'
+NUM = r'(?P<NUM>\d+)'
+PLUS = r'(?P<PLUS>\+)'
+MINUS = r'(?P<MINUS>-)'
+TIMES = r'(?P<TIMES>\*)'
 DIVIDE = r'(?P<DIVIDE>/)'
 LPAREN = r'(?P<LPAREN>\()'
 RPAREN = r'(?P<RPAREN>\))'
-WS     = r'(?P<WS>\s+)'
+WS = r'(?P<WS>\s+)'
 
-master_pat = re.compile('|'.join([NUM, PLUS, MINUS, TIMES, 
+master_pat = re.compile('|'.join([NUM, PLUS, MINUS, TIMES,
                                   DIVIDE, LPAREN, RPAREN, WS]))
 
 # Tokenizer
-Token = collections.namedtuple('Token', ['type','value'])
+Token = collections.namedtuple('Token', ['type', 'value'])
 
-def generate_tokens(text):
-    scanner = master_pat.scanner(text)
+
+def generate_tokens(text, pat):
+    scanner = pat.scanner(text)
     for m in iter(scanner.match, None):
         tok = Token(m.lastgroup, m.group())
         if tok.type != 'WS':
             yield tok
 
-# Parser 
-class ExpressionEvaluator:
-    '''
-    Implementation of a recursive descent parser.   Each method
-    implements a single grammar rule.  Use the ._accept() method
-    to test and accept the current lookahead token.  Use the ._expect()
-    method to exactly match and discard the next token on on the input
-    (or raise a SyntaxError if it doesn't match).
-    '''
+# Parser
 
-    def parse(self,text):
-        self.tokens = generate_tokens(text)
+
+class ExpressionEvaluator:
+    """Implementation of a recursive descent parser.
+
+        Each method implements a single grammar rule.  Use the ._accept()
+    method to test and accept the current lookahead token.
+        Use the ._expect() method to exactly match and discard the next
+    token on on the input (or raise a SyntaxError if it doesn't match).
+    """
+
+    def parse(self, text):
+        self.tokens = generate_tokens(text, master_pat)
         self.tok = None             # Last symbol consumed
         self.nexttok = None         # Next symbol tokenized
         self._advance()             # Load first lookahead token
         return self.expr()
 
     def _advance(self):
-        'Advance one token ahead'
+        """Advance one token ahead."""
         self.tok, self.nexttok = self.nexttok, next(self.tokens, None)
 
-    def _accept(self,toktype):
-        'Test and consume the next token if it matches toktype'
+    def _accept(self, toktype):
+        """Test and consume the next token if it matches toktype."""
         if self.nexttok and self.nexttok.type == toktype:
             self._advance()
             return True
         else:
             return False
 
-    def _expect(self,toktype):
-        'Consume next token if it matches toktype or raise SyntaxError'
+    def _expect(self, toktype):
+        """Consume next token if it matches toktype or raise SyntaxError."""
         if not self._accept(toktype):
             raise SyntaxError('Expected ' + toktype)
 
     # Grammar rules follow
 
     def expr(self):
-        "expression ::= term { ('+'|'-') term }*"
-
+        """Expression ::= term { ('+'|'-') term }*."""
         exprval = self.term()
         while self._accept('PLUS') or self._accept('MINUS'):
             op = self.tok.type
@@ -76,10 +80,9 @@ class ExpressionEvaluator:
             elif op == 'MINUS':
                 exprval -= right
         return exprval
-    
-    def term(self):
-        "term ::= factor { ('*'|'/') factor }*"
 
+    def term(self):
+        """Term ::= factor { ('*'|'/') factor }*."""
         termval = self.factor()
         while self._accept('TIMES') or self._accept('DIVIDE'):
             op = self.tok.type
@@ -91,8 +94,7 @@ class ExpressionEvaluator:
         return termval
 
     def factor(self):
-        "factor ::= NUM | ( expr )"
-
+        """Factor ::= NUM | ( expr )."""
         if self._accept('NUM'):
             return int(self.tok.value)
         elif self._accept('LPAREN'):
@@ -101,6 +103,7 @@ class ExpressionEvaluator:
             return exprval
         else:
             raise SyntaxError('Expected NUMBER or LPAREN')
+
 
 if __name__ == '__main__':
     e = ExpressionEvaluator()
@@ -111,10 +114,10 @@ if __name__ == '__main__':
 
 # Example of building trees
 
+
 class ExpressionTreeBuilder(ExpressionEvaluator):
     def expr(self):
-        "expression ::= term { ('+'|'-') term }"
-
+        """Expression ::= term { ('+'|'-') term }."""
         exprval = self.term()
         while self._accept('PLUS') or self._accept('MINUS'):
             op = self.tok.type
@@ -124,10 +127,9 @@ class ExpressionTreeBuilder(ExpressionEvaluator):
             elif op == 'MINUS':
                 exprval = ('-', exprval, right)
         return exprval
-    
-    def term(self):
-        "term ::= factor { ('*'|'/') factor }"
 
+    def term(self):
+        """Term ::= factor { ('*'|'/') factor }."""
         termval = self.factor()
         while self._accept('TIMES') or self._accept('DIVIDE'):
             op = self.tok.type
@@ -139,8 +141,7 @@ class ExpressionTreeBuilder(ExpressionEvaluator):
         return termval
 
     def factor(self):
-        'factor ::= NUM | ( expr )'
-
+        """Factor ::= NUM | ( expr )."""
         if self._accept('NUM'):
             return int(self.tok.value)
         elif self._accept('LPAREN'):
@@ -149,6 +150,7 @@ class ExpressionTreeBuilder(ExpressionEvaluator):
             return exprval
         else:
             raise SyntaxError('Expected NUMBER or LPAREN')
+
 
 if __name__ == '__main__':
     e = ExpressionTreeBuilder()
--- src/2 - strings and texts/2.19-writing_a_simple_recursive_descent_parser/plyexample.py
+++ src/2 - strings and texts/2.19-writing_a_simple_recursive_descent_parser/plyexample.py
@@ -6,35 +6,42 @@ from ply.lex import lex
 from ply.yacc import yacc
 
 # Token list
-tokens = [ 'NUM', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN' ]
+tokens = ['NUM', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN']
 
 # Ignored characters
 
 t_ignore = ' \t\n'
 
 # Token specifications (as regexs)
-t_PLUS   = r'\+'
-t_MINUS  = r'-'
-t_TIMES  = r'\*'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 
 # Token processing functions
+
+
 def t_NUM(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
 # Error handler
+
+
 def t_error(t):
     print('Bad character: {!r}'.format(t.value[0]))
     t.skip(1)
+
 
 # Build the lexer
 lexer = lex()
 
 # Grammar rules and handler functions
+
+
 def p_expr(p):
     '''
     expr : expr PLUS term
@@ -45,11 +52,13 @@ def p_expr(p):
     elif p[2] == '-':
         p[0] = p[1] - p[3]
 
+
 def p_expr_term(p):
     '''
     expr : term
     '''
     p[0] = p[1]
+
 
 def p_term(p):
     '''
@@ -61,11 +70,13 @@ def p_term(p):
     elif p[2] == '/':
         p[0] = p[1] / p[3]
 
+
 def p_term_factor(p):
     '''
     term : factor
     '''
     p[0] = p[1]
+
 
 def p_factor(p):
     '''
@@ -73,14 +84,17 @@ def p_factor(p):
     '''
     p[0] = p[1]
 
+
 def p_factor_group(p):
     '''
     factor : LPAREN expr RPAREN
     '''
     p[0] = p[2]
 
+
 def p_error(p):
     print('Syntax error')
+
 
 parser = yacc()
 
