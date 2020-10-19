55b22c6c3a7062ee93b3ab53456f809b7fea04d5
ralphribeiro <ralph_ribeiro@msn.com>
2020-10-19 12:17:07 -0300

    refatoração

--- src/4 - iterators and generators/4.10-iterate_over_the_index-value_pairs_of_a_list/example.py
+++ src/4 - iterators and generators/4.10-iterate_over_the_index-value_pairs_of_a_list/example.py
@@ -1,11 +1,19 @@
+from os.path import abspath, dirname, join
+
+
+p = dirname(abspath(__file__))
+
 # Example of iterating over lines of a file with an extra lineno attribute
+
+
 def parse_data(filename):
     with open(filename, 'rt') as f:
-         for lineno, line in enumerate(f, 1):
-             fields = line.split()
-             try:
-                 count = int(fields[1])
-             except ValueError as e:
-                 print('Line {}: Parse error: {}'.format(lineno, e))
+        for lineno, line in enumerate(f, 1):
+            fields = line.split()
+            try:
+                count = int(fields[1])
+            except ValueError as e:
+                print(f'Line {lineno}: Parse error: {e}')
 
-parse_data('sample.dat')
+
+parse_data(join(p, 'sample.dat'))
