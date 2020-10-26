b145f3846abdc23b139ba9efb022e99f17184dae
ralphribeiro <ralph_ribeiro@msn.com>
2020-10-26 14:41:33 -0300

    corrigido indentação

--- src/8 - classes and objects/8.14-implementing_custom_containers/example1.py
+++ src/8 - classes and objects/8.14-implementing_custom_containers/example1.py
@@ -3,6 +3,7 @@
 import collections
 import bisect
 
+
 class SortedItems(collections.Sequence):
     def __init__(self, initial=None):
         self._items = sorted(initial) if initial is not None else []
@@ -18,17 +19,18 @@ class SortedItems(collections.Sequence):
     def add(self, item):
         bisect.insort(self._items, item)
 
+
 if __name__ == '__main__':
-   items = SortedItems([5, 1, 3])
-   print(list(items))
-   print(items[0])
-   print(items[-1])
-   items.add(2)
-   print(list(items))
-   items.add(-10)
-   print(list(items))
-   print(items[1:4])
-   print(3 in items)
-   print(len(items))
-   for n in items:
-       print(n)
+    items = SortedItems([5, 1, 3])
+    print(list(items))
+    print(items[0])
+    print(items[-1])
+    items.add(2)
+    print(list(items))
+    items.add(-10)
+    print(list(items))
+    print(items[1:4])
+    print(3 in items)
+    print(len(items))
+    for n in items:
+        print(n)
